79f9521c2c5e8e62d4ef3c983fa6899cc45c4e08
Aryan <as1991218113@gmail.com>
2024-10-19 20:54:17 +0530

    UI changes

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,11 @@ export default function Home() {
         <h2 className="text-xl font-thin my-2">Seamless Entry Management for Students of IIT Ropar</h2>
         <div className="w-full h-full flex">
           <div className="flex-1 flex justify-center items-center">
-            <button className="bg-green-500 m-10 text-4xl rounded-xl hover:bg-green-800 hover:translate-y-3 p-16" style={{width:"450px", height:"300px"}} onClick={()=>{router.push("/student_entry")}}>Student Entry <span className="font-thin"><br/>(ID Card Required)</span></button>
+            <button className="bg-green-500 m-10 text-3xl rounded-xl hover:bg-green-800 hover:translate-y-3 p-16" style={{width:"400px", height:"270px"}} onClick={()=>{router.push("/student_entry_home")}}>Record Home Entry<span className="font-thin"><br/>(ID Card Required)</span></button>
+            <button className="bg-green-500 m-10 text-3xl rounded-xl hover:bg-green-800 hover:translate-y-3 p-14" style={{width:"400px", height:"270px"}} onClick={()=>{router.push("/student_entry_regular")}}> Record Regular Entry  <span className="font-thin"><br/>(ID Card Required)</span></button>
           {/* </div>
           <div className="flex-1 flex justify-center items-center"> */}
-          <button className="bg-green-500 m-10 text-4xl rounded-xl hover:bg-green-800 hover:translate-y-3 p-16" style={{width:"450px", height:"300px"}} onClick={()=>{router.push("/guest_entry")}}>Guest Entry</button>
+          <button className="bg-green-500 m-10 text-3xl rounded-xl hover:bg-green-800 hover:translate-y-3 p-16" style={{width:"400px", height:"270px"}} onClick={()=>{router.push("/guest_entry")}}>Guest Entry</button>
           </div>
           </div>
       </header>
