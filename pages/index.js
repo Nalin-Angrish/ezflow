1142454615d1bf8573f87ab9d23226ce7d722758
Aryan <as1991218113@gmail.com>
2024-10-20 00:04:47 +0530

    Logos

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,13 @@ export default function Home() {
         <title>EZFlow</title>
       </Head>
       <header className="w-full text-center p-4 h-full flex justify-center items-center flex-col" style={{height:"100vh"}}>
-        <h1 className="text-5xl font-bold my-4">EZFlow</h1>
+      <div style={{ position: "absolute", top: "30px", left: "20px" }}>
+          <img src="/logo.png" alt="IIT Ropar" className="w-35 h-32" />
+        </div>
+        <div style={{position:  "absolute", top: "30px", right: "20px"}}>
+          <img src="./darkbg.png" alt="Softcom" className="w-24 h-auto" />
+        </div>
+        <h1 className="text-5xl font-bold my-4">IIT Ropar Entry Management System</h1>
         <h2 className="text-xl font-thin my-2">Seamless Entry Management for Students of IIT Ropar</h2>
         <div className="w-full h-full flex">
           <div className="flex-1 flex justify-center items-center">
